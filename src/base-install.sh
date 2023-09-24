bd52a159bb27f67e011f50a0978c014c28130e42
zodiac403 <zodiac403@gmx.de>
2023-09-24 11:41:13 +0200

    Create base-install.sh

---
+++ src/base-install.sh
@@ -0,0 +1,4 @@
+echo "== INSTALL BASE TOOLING =="
+
+echo "=== INSTALL HOMEBREW ==="
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
