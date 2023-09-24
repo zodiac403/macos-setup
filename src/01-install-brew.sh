37f6af17891e3a8d0d5582919b15960dc4463fff
zodiac403 <zodiac403@gmx.de>
2023-09-24 13:12:33 +0200

    Update and rename base-install.sh to 01-install-brew.sh

--- src/01-install-brew.sh
+++ src/01-install-brew.sh
@@ -2,3 +2,5 @@ echo "== INSTALL BASE TOOLING =="
 
 echo "=== INSTALL HOMEBREW ==="
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "== SUCCESS =="
