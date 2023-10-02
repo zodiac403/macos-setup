b1bb072e9cd20ed52d9d1e6ad3f6886fd37c6def
zodiac403 <zodiac403@gmx.de>
2023-10-02 06:38:32 +0200

    Create 03-install-python.sh

---
+++ src/03-install-python.sh
@@ -0,0 +1,29 @@
+echo "== INSTALL PYTHON =="
+
+PYTHON_VERSION="3.9"
+
+echo "=== BREW INSTALL ==="
+brew update && brew upgrade
+brew install pyenv
+
+echo "=== PYENV INSTALL ==="
+pyenv init -
+pyenv install $PYTHON_VERSION
+pyenv versions
+
+echo "=== PIP INSTALL ==="
+pip3 install \
+  autopep8 \
+  pylint
+
+echo "=== CODIUM INSTALL EXTENSIONS ==="
+for EXTENSION in \
+  ms-python.python \
+  ms-python.isort \
+  ms-python.pylint \
+  LittleFoxTeam.vscode-python-test-adapter
+do
+	codium --install-extension $EXTENSION
+done
+
+echo "== SUCCESS =="
