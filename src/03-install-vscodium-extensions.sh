b0a000546fcd5bc8a43ea08ebd6d80b74a451a08
zodiac403 <zodiac403@gmx.de>
2023-10-02 14:34:50 +0200

    Create 03-install-vscodium-extensions.sh

---
+++ src/03-install-vscodium-extensions.sh
@@ -0,0 +1,28 @@
+echo "== INSTALL COMMON EXTENSIONS FOR VS CODIUM =="
+
+echo "=== INSTALL COMMON EXTENSIONS ==="
+for EXTENSION in \
+  dbaeumer.vscode-eslint \
+  EditorConfig.EditorConfig \
+  emilast.LogFileHighlighter \
+  esbenp.prettier-vscode \
+  mechatroner.rainbow-csv \
+  streetsidesoftware.code-spell-checker \
+do
+  codium --install-extension $EXTENSION
+done
+
+echo "=== INSTALL MARKDOWN EXTENSIONS ==="
+for EXTENSION in \
+  bierner.markdown-preview-github-styles \
+  darkriszty.markdown-table-prettify \
+  DavidAnson.vscode-markdownlint \
+  huntertran.auto-markdown-toc \
+  marp-team.marp-vscode \
+  sugatoray.vscode-markdown-extension-pack \
+  yzane.markdown-pdf \
+do
+  codium --install-extension $EXTENSION
+done
+
+echo "== SUCCESS =="
