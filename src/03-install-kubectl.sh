b15bc02f6a6394204d208b4594c2f0974521c0bf
zodiac403 <zodiac403@protonmail.com>
2025-03-06 19:47:52 +0100

    Kubectl: Fix bashrc script

--- src/03-install-kubectl.sh
+++ src/03-install-kubectl.sh
@@ -1,4 +1,4 @@
-echo "== INSTALL JAVA AND KOTLIN =="
+echo "== INSTALL KUBECTL =="
 
 echo ""
 echo "=== BREW INSTALL ==="
@@ -10,23 +10,25 @@ echo ""
 echo "=== CONFIGURE KUBECTL ALIASES ==="
 echo "-> reference: <https://github.com/ahmetb/kubectl-aliases>"
 ALIASES_FILE="~/.kubectl_aliases"
+echo "download aliases file to '${ALIASES_FILE}'"
 curl -fs https://raw.githubusercontent.com/ahmetb/kubectl-aliases/refs/heads/master/.kubectl_aliases > ${ALIASES_FILE}
 
 for RC_FILE in \
     "~/.bashrc" \
     "~/.zshrc"; do
-    if [-f ${RC_FILE} ]; then
-        echo "configure ${RC_FILE}"
+    if [ -f ${RC_FILE} ]; then
+        echo "configure '${RC_FILE}'"
         cp ${RC_FILE} ${RC_FILE}.backup
-        echo "" >> ${RC_FILE}
-        echo "# kubectl aliases" >> ${RC_FILE}
-        echo "[ -f ${ALIASES_FILE} ] && source ${ALIASES_FILE}" >> ${RC_FILE}
-        # echo "function kubectl() { echo "+ kubectl $@">&2; command kubectl $@; }" >> ${RC_FILE}
-        echo "" >> ${RC_FILE}
+        cat << EOF >> ${RC_FILE}
 
+# kubectl aliases
+[ -f ${ALIASES_FILE} ] && source ${ALIASES_FILE}
+function kubectl() { echo "+ kubectl \$@">&2; command kubectl \$@; }
+EOF
         source ${RC_FILE}
+    else
+        echo "skip '${RC_FILE}' (not present)"
     fi
-
 done
 
 echo ""
