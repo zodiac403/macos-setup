75b556c7dc84e144a63bd19588c4036eec992c88
zodiac403 <zodiac403@protonmail.com>
2025-03-06 19:46:22 +0100

    AWS CDK: Fix bash script syntax

--- src/03-install-aws-cdk-with-podman.sh
+++ src/03-install-aws-cdk-with-podman.sh
@@ -14,13 +14,13 @@ BIN_DOCKER_PATH = "/usr/local/bin/docker"
 cat << EOF > $BIN_DOCKER_PATH
 #!/usr/bin/env bash
 
-podman "$@"
+podman "\$@"
 
-if [ $? == 125 ]; then
+if [ \$? == 125 ]; then
   echo "WARNING: Podman returned code 125, exit with code 1"
   echo "         When this happens during cdk deploy,"
   echo "         this is probably fine. Otherwise inspect"
-  ehco "         the output above."
+  echo "         the output above."
   exit 1;
 fi
 EOF
